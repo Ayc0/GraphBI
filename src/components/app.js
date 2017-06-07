0066c1feb7bbe4bf6f767f751436e8dc7e477377
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-06-07 13:01:02 +0200

    Feat: Add block component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Block from './block';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+const LeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+`;
+
+const RightColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1em;
+  flex-grow: 1;
+`;
 
 export default () =>
-  <div>
-    <p>Hey !</p>
-  </div>;
+  <Container>
+    <LeftColumn>
+      <Block>Hey !</Block>
+      <Block>Hey !</Block>
+      <Block>Hey !</Block>
+    </LeftColumn>
+    <RightColumn>
+      <p>Yo</p>
+      <p>Yo</p>
+      <p>Yo</p>
+      <p>Yo</p>
+    </RightColumn>
+  </Container>;
---
+++ src/components/block.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  background-color: white;
+  margin-bottom: 1em;
+  padding: 1em;
+  box-shadow: 0px 0px 0px 0.5px rgba(47, 47, 47, 0.27);
+`;
