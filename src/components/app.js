d7bd30c1dd0370df59f7f0ac1d9b4c274330e7ad
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-08 14:49:04 +0200

    started adding function to manage data

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Container, LeftColumn, RightColumn } from "../styles/layout";
 import { Block, BlockTitle } from "../styles/block";
 
 import Chart from "./chart";
 
-export default () =>
-  <Container>
-    <LeftColumn>
-      <Block>
-        <BlockTitle>Hey!</BlockTitle>
-        <p>Plop</p>
-      </Block>
-      <Block>Hey !</Block>
-      <Block>Hey !</Block>
-    </LeftColumn>
-    <RightColumn>
-      <Chart />
-    </RightColumn>
-  </Container>;
+const data = require("../data/Projects.json");
+const columns = data[0];
+const values = data[1];
+
+function getBrandAndRevenue(value) {
+  let Brand = value.Brand ? value.Brand : 'None';
+  let Revenue = value["Revenues (k€)"] ? value["Revenues (k€)"] : 0;
+  return [Brand, Revenue]
+}
+
+function getRevenuePerBrand(array) {
+  const BrandAndRevenue = array.map(getBrandAndRevenue);
+  let RevenuePerBrand = {};
+  for (let i=0 ; i < array.length ; i++) {
+    let item = BrandAndRevenue[i];
+    RevenuePerBrand[item[0]] = RevenuePerBrand[item[0]] ? item[1] + RevenuePerBrand[item[0]] : item[1]
+  }
+  console.log(RevenuePerBrand);
+  return RevenuePerBrand
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <LeftColumn>
+          <Block>
+            <BlockTitle>Hey!</BlockTitle>
+            <p>Plop</p>
+          </Block>
+          <Block>Hey !</Block>
+          <Block>Hey !</Block>
+        </LeftColumn>
+        <RightColumn>
+          <Chart data={getRevenuePerBrand(values)} />
+        </RightColumn>
+      </Container>
+    );
+  }
+}
