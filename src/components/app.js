63b0c1c722c6f55584173ab1474fd750498fdf64
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-09 11:37:50 +0200

    added dropdown

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container, LeftColumn, RightColumn } from '../styles/layout';
 import { Block, BlockTitle } from '../styles/block';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 import json from '../data/projects.json';
 
@@ -10,33 +12,46 @@ import Chart from './chart';
 import filterXAxis from '../functions/filterXAxis';
 import sumYAxis from '../functions/sumYAxis';
 
-console.log(json);
-
 const values = json[1];
+
+const options = json[0].map((column) => {return {"label": column.title, "value": column.title}});
 
 const first = filterXAxis(values, 'Brand');
 
-const data = sumYAxis(first, ['Latitude', 'Longitude']);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'options': options,
+      'selected_value': options[0],
+    }
+  }
 
-console.log(data);
-
-export default () =>
-  (<Container>
-    <LeftColumn>
-      <Block>
-        <BlockTitle>Y Axis :</BlockTitle>
-        <p>Sum of Revenue</p>
-      </Block>
-      <Block>
-        <BlockTitle>X Axis :</BlockTitle>
-        <p>by Brand</p>
-      </Block>
-      <Block>
-        <BlockTitle>Type of graph :</BlockTitle>
-        <p>Linear</p>
-      </Block>
-    </LeftColumn>
-    <RightColumn>
-      <Chart data={data} />
-    </RightColumn>
-  </Container>);
+  render() {
+    return (<Container>
+      <LeftColumn>
+        <Block>
+          <BlockTitle>Y Axis :</BlockTitle>
+          <p>Sum of Revenue</p>
+          <Select
+            name="form-field-name"
+            value={this.state.selected_value}
+            options={options}
+            onChange={(e) => {this.setState({'selected_value': e.label})}}
+          />
+        </Block>
+        <Block>
+          <BlockTitle>X Axis :</BlockTitle>
+          <p>by Brand</p>
+        </Block>
+        <Block>
+          <BlockTitle>Type of graph :</BlockTitle>
+          <p>Linear</p>
+        </Block>
+      </LeftColumn>
+      <RightColumn>
+        <Chart data={sumYAxis(first, [this.state.selected_value])} />
+      </RightColumn>
+    </Container>)
+  }
+}
