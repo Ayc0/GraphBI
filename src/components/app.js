157d31c9f422d9327f99c55e6c54f914e897d306
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-12 15:53:13 +0200

    render graph conditionally

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import { Container, LeftColumn, RightColumn } from '../styles/layout';
 import GraphTypeBlock from './graph-type-block';
 import FirstAxisBlock from './first-axis-block';
 import SecondAxisBlock from './second-axis-block';
+import SimplePieChart from './piechart';
 
 import json from '../data/projects.json';
 
@@ -27,9 +28,14 @@ export default class App extends Component {
     super(props);
     this.state = {
       options,
+      graph_type: 'Linear',
       Y_selected_value: [],
       X_selected_value: options[1],
     };
+  }
+
+  onGraphTypeChange(e) {
+    this.setState({ graph_type: e });
   }
 
   onFirstAxisChange(e) {
@@ -40,27 +46,30 @@ export default class App extends Component {
     this.setState({ X_selected_value: e });
   }
 
+  renderGraph(selection) {
+    if (selection === 'PieChart') {
+      return <SimplePieChart />;
+    }
+    return (
+      <Chart
+        data={meanYAxis(
+          filterXAxis(values, this.state.X_selected_value),
+          this.state.Y_selected_value.map(option => option.label),
+        )}
+      />
+    );
+  }
+
   render() {
     return (
       <Container>
         <LeftColumn>
-          <GraphTypeBlock />
-          <FirstAxisBlock
-            options={options}
-            onFirstAxisChange={e => this.onFirstAxisChange(e)}
-          />
-          <SecondAxisBlock
-            options={options}
-            onSecondAxisChange={e => this.onSecondAxisChange(e)}
-          />
+          <GraphTypeBlock onGraphTypeChange={e => this.onGraphTypeChange(e)} />
+          <FirstAxisBlock options={options} onFirstAxisChange={e => this.onFirstAxisChange(e)} />
+          <SecondAxisBlock options={options} onSecondAxisChange={e => this.onSecondAxisChange(e)} />
         </LeftColumn>
         <RightColumn>
-          <Chart
-            data={meanYAxis(
-              filterXAxis(values, this.state.X_selected_value),
-              this.state.Y_selected_value.map(option => option.label),
-            )}
-          />
+          {this.renderGraph(this.state.graph_type)}
         </RightColumn>
       </Container>
     );
--- src/components/graph-type-block.js
+++ src/components/graph-type-block.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
 import { Block, BlockTitle } from '../styles/block';
 
-const GraphTypeBlock = () =>
-  (<Block>
-    <BlockTitle>Type of graph :</BlockTitle>
-    <p>Linear</p>
-  </Block>);
+const options = [{ value: 'Linear', label: 'Linear' }, { value: 'PieChart', label: 'PieChart' }];
+
+class GraphTypeBlock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected_value: options[0],
+    };
+  }
+
+  onSelectChange(e) {
+    this.setState({ selected_value: e });
+    this.props.onGraphTypeChange(e);
+  }
+
+  render() {
+    return (
+      <Block>
+        <BlockTitle>Type of graph :</BlockTitle>
+        <Select
+          name="graph-type"
+          value={this.state.selected_value}
+          options={options}
+          onChange={e => this.onSelectChange(e.label)}
+        />
+      </Block>
+    );
+  }
+}
 
 export default GraphTypeBlock;
