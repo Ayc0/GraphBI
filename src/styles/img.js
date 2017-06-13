faec206a7678ec25800d6ec54863a49fd73d9633
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-06-13 11:43:37 +0200

    Feat: Icon hover

--- src/styles/img.js
+++ src/styles/img.js
@@ -3,14 +3,19 @@ import styled from 'styled-components';
 const Img = styled.img`
   height: ${({ size }) => size};
   width: ${({ size }) => size};
-  margin-right: 0.6em;
+  padding: 0.3em;
+  margin-right: 0.3em;
   :last-child {
     margin-right: 0;
   };
+  :hover {
+    cursor: pointer;
+    background-color: #edf0f2;
+  }
 `;
 
 Img.defaultProps = {
-  size: '4em',
+  size: '3.6em',
 };
 
 export default Img;
