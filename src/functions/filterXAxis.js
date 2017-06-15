ebbd9d0618377ba5ddcc6fd99767f5ff540bea25
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-06-15 11:37:46 +0200

    Fix: Don't show undefined label in graph

--- src/functions/filterXAxis.js
+++ src/functions/filterXAxis.js
@@ -2,10 +2,11 @@ import { groupBy } from 'lodash';
 
 const filterXAxis = (json, xAxis) => {
   const groupedBy = groupBy(json, element => element[xAxis]);
-  return Object.keys(groupedBy).map(key => ({
+  const out = Object.keys(groupedBy).map(key => ({
     name: key,
     values: groupedBy[key],
   }));
+  return out.filter(element => element.name !== 'undefined');
 };
 
 export default filterXAxis;
