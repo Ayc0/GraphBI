13e2af80cc569ed4484f5e4907a67baf8e0e97d3
Ayc0 <benjamin.koltes@gmail.com>
2017-06-19 15:28:12 +0200

    feat: can plot by any number marked label

--- src/functions/filterXAxis.js
+++ src/functions/filterXAxis.js
@@ -3,11 +3,22 @@ import sortBy from 'lodash/sortBy';
 
 import weekConverter from './weekConverter';
 
+import data from '../data/projects.json';
+
+const numberLabels = data[0]
+  .filter(column => column.type === 'number')
+  .map(column => column.title);
+
 // INPUT : a JSON and the label of the columns used as an abscissa
 // OUTPUT : a list of {name: '', values: [...]} elements
 // where each name corresponds to a value of the label
 // (ex: Bike, Run-In and Swim Fast for the label Brand)
 // and values regroups the elements of the JSON containing the associate label
+
+const toNumber = (string) => {
+  const nb = parseFloat(string);
+  return isNaN(nb) ? 0 : nb;
+};
 
 const filterXAxis = (json, xAxis) => {
   const groupedBy = groupBy(json, element => element[xAxis]);
@@ -24,6 +35,15 @@ const filterXAxis = (json, xAxis) => {
           return newElement;
         })
         .filter(element => !isNaN(element.name)),
+      ['name'],
+    );
+  }
+  if (numberLabels.includes(xAxis)) {
+    return sortBy(
+      out.filter(element => element.name !== 'undefined').map(element => ({
+        name: toNumber(element.name),
+        values: element.values,
+      })),
       ['name'],
     );
   }
