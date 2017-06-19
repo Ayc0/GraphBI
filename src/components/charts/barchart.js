6cf024c9a3811b5acacadd3237769a750de936cd
Ayc0 <benjamin.koltes@gmail.com>
2017-06-19 16:24:08 +0200

    chore: lint barchart.js

--- src/components/charts/barchart.js
+++ src/components/charts/barchart.js
@@ -13,13 +13,14 @@ const color = ['#01b8aa', '#374649', '#f2c80f', '#fd625e', '#5f6b6d'];
 
 const SimpleBarChart = ({ data }) =>
   (<ResponsiveContainer>
-    <BarChart style={{ height: 'auto', width: '100%' }}
-              data={data}
-              margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-      <XAxis dataKey="name"
-             domain={['dataMin', 'dataMax']} />
-      <YAxis/>
-      <CartesianGrid strokeDasharray="3 3"/>
+    <BarChart
+      style={{ height: 'auto', width: '100%' }}
+      data={data}
+      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+    >
+      <XAxis dataKey="name" domain={['dataMin', 'dataMax']} />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
       <Tooltip />
       {Object.keys(data[0] || {})
         .filter(element => element !== 'name')
