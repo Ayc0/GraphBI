a24ad0010bfa1d18c9879fa8fa662aede9a89a53
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-19 15:15:53 +0200

    barchart working

--- src/components/charts/barchart.js
+++ src/components/charts/barchart.js
@@ -6,16 +6,10 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from 'recharts';
 
 const color = ['#01b8aa', '#374649', '#f2c80f', '#fd625e', '#5f6b6d'];
-
-const dateFormatter = (tick) => {
-  const date = new Date(tick);
-  return date.toLocaleDateString();
-};
 
 const SimpleBarChart = ({ data }) =>
   (<ResponsiveContainer>
@@ -23,13 +17,10 @@ const SimpleBarChart = ({ data }) =>
               data={data}
               margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
       <XAxis dataKey="name"
-             type="number"
-             domain={['dataMin', 'dataMax']}
-             tickFormatter={dateFormatter}/>
+             domain={['dataMin', 'dataMax']} />
       <YAxis/>
       <CartesianGrid strokeDasharray="3 3"/>
-      <Tooltip labelFormatter={dateFormatter} />
-      <Legend />
+      <Tooltip />
       {Object.keys(data[0] || {})
         .filter(element => element !== 'name')
         .map((element, id) =>
@@ -38,6 +29,7 @@ const SimpleBarChart = ({ data }) =>
             dataKey={element}
             fill={color[id]}
             fillOpacity={0.3}
+            stroke={color[id]}
           />),
         )}
     </BarChart>
