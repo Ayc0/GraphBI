50e17b92b486665dda77381f7c463a59de795cbf
Ayc0 <benjamin.koltes@gmail.com>
2017-06-20 10:02:34 +0200

    refactor: simplify countYAxis

--- src/functions/countYAxis.js
+++ src/functions/countYAxis.js
@@ -2,13 +2,10 @@
 // OUTPUT : list containing {name: '', count: x} elements
 // With 'count' being the number of occurrences of 'name' in the JSON
 
-const countYAxis = (json) => {
-  const out = [];
-  json.forEach((field, id) => {
-    out.push({ name: field.name });
-    out[id].value = field.values.length;
-  });
-  return out;
-};
+const countYAxis = json =>
+  json.map(element => ({
+    name: element.name,
+    value: element.values.length,
+  }));
 
 export default countYAxis;
