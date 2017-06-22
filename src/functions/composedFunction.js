3c0eebe750f1598fd9d5d3c5a9f87f56c7162dc4
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-22 16:50:32 +0200

    feat: composed-chart works with all functions

--- src/functions/composedFunction.js
+++ src/functions/composedFunction.js
@@ -1,13 +1,35 @@
+import sumBy from 'lodash/sumBy';
+import meanBy from 'lodash/meanBy';
+
+const findRightFunction = (func) => {
+  switch (func) {
+    case 'sum':
+      return sumBy;
+    case 'avg':
+      return meanBy;
+    default:
+      return values => (values || []).length;
+  }
+};
+
 export default (data, YSelected1, function1, YSelected2, function2) => {
   const label1 = function1 === 'number' ? 'number' : `${function1}_${YSelected1}`;
   const label2 = function2 === 'number' ? 'number' : `${function2}_${YSelected2}`;
   const out = [];
+  const func1 = findRightFunction(function1);
+  const func2 = findRightFunction(function2);
   data.forEach((field, index) => {
     out.push({
       name: field.name,
     });
-    out[index][label1] = 2;
-    out[index][label2] = 4;
+    out[index][label1] = func1(field.values, (element) => {
+      const elementInt = parseInt(element[YSelected1], 10);
+      return isNaN(elementInt) ? 0 : elementInt;
+    });
+    out[index][label2] = func2(field.values, (element) => {
+      const elementInt = parseInt(element[YSelected1], 10);
+      return isNaN(elementInt) ? 0 : elementInt;
+    });
   });
   return out;
 };
