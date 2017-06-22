697fa06d4d56e6c87e28632f15190684ef6ab530
Ayc0 <benjamin.koltes@gmail.com>
2017-06-22 11:54:09 +0200

    style: change aspect of icons

--- src/styles/img.js
+++ src/styles/img.js
@@ -1,28 +1,26 @@
 import styled from 'styled-components';
 
 const Img = styled.img`
-  margin-right: 0.3em;
   padding: 3px;
-  :last-child {
-    margin-right: ${({ active }) => (active ? '4px' : '5px')};
-  };
+  margin: ${({ active }) => (active ? '2px' : '3px')};
+  margin-top: 5px;
+  margin-bottom: 5px;
   border: ${({ active }) => (active ? 'dashed 1px rgb(204, 204, 204)' : '')};
-  margin: ${({ active }) => (active ? '4px' : '5px')};
   :hover {
     cursor: pointer;
     background-color: #edf0f2;
   };
 
-  height: calc(25% - 16px);
-  width: calc(25% - 16px);
+  height: calc(20% - 12px);
+  width: calc(20% - 12px);
 
   @media (max-width: 576px) {
-    height: calc(50% - 16px);
-    width: calc(50% - 16px);
+    height: calc(50% - 12px);
+    width: calc(50% - 12px);
   };
   @media (min-width: 577px) and (max-width: 750px) {
-    height: calc(33% - 16px);
-    width: calc(33% - 16px);
+    height: calc(33% - 12px);
+    width: calc(33% - 12px);
   };
 `;
 
