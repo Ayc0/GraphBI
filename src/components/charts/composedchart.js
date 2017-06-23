965b5398eec850bbad4c16ec181d0995d05743ce
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-23 13:41:06 +0200

    fix: merge

--- src/components/charts/composedchart.js
+++ src/components/charts/composedchart.js
@@ -22,12 +22,8 @@ export default ({
   YSelected2,
   functionSelected2,
 }) => {
-  const label1 = functionSelected === 'number'
-    ? 'number'
-    : `${functionSelected}_${YSelected}`;
-  const label2 = functionSelected2 === 'number'
-    ? 'number'
-    : `${functionSelected2}_${YSelected2}`;
+  const label1 = functionSelected === 'number' ? 'number' : `${functionSelected}_${YSelected}`;
+  const label2 = functionSelected2 === 'number' ? 'number' : `${functionSelected2}_${YSelected2}`;
   return (
     <ResponsiveContainer>
       <ComposedChart
@@ -37,7 +33,8 @@ export default ({
         margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
       >
         {xAxis(XSelected)}
-        <YAxis />
+        <YAxis yAxisId="left" dataKey={label1} />
+        <YAxis yAxisId="right" dataKey={label2} orientation="right" />
         <Tooltip labelFormatter={checkTickFormater(XSelected)} />
         {legend}
         <CartesianGrid stroke="#f5f5f5" />
@@ -48,7 +45,9 @@ export default ({
               key={`bar${element}`}
               dataKey={label1}
               fill={color[id]}
+              fillOpacity={0.3}
               stroke={color[id]}
+              yAxisId="left"
             />),
           )}
         {Object.keys(data[0] || {})
@@ -58,7 +57,9 @@ export default ({
               type="monotone"
               key={`line${element}`}
               dataKey={label2}
+              fillOpacity={0.3}
               stroke={color[id + 2]}
+              yAxisId="right"
             />),
           )}
       </ComposedChart>
