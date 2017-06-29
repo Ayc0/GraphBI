4eda18bd59b3b605803f8f0fa2e2182fbd1d1a50
Solen Le Roux--Couloigner <solen.leroux.c@gmail.com>
2017-06-29 11:55:27 +0200

    fix: better 'st' 'nd' and 'rd' after numbers

--- src/functions/dateDisplayer.js
+++ src/functions/dateDisplayer.js
@@ -1,5 +1,18 @@
 const monthNumbers = number => (number > 9 ? number.toString() : `0${number.toString()}`);
-const numberAdj = number => (number < 4 ? ['st', 'nd', 'rd'][number - 1] : 'th');
+const numberAdj = (i) => {
+  const j = i % 10;
+  const k = i % 100;
+  if (j === 1 && k !== 11) {
+    return 'st';
+  }
+  if (j === 2 && k !== 12) {
+    return 'nd';
+  }
+  if (j === 3 && k !== 13) {
+    return 'rd';
+  }
+  return 'th';
+};
 
 const getISOWeekFromDate = (date) => {
   const onejan = new Date(date.getFullYear(), 0, 1);
